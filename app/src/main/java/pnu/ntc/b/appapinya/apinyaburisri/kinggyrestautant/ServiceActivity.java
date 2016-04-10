f4f42993eb1261bb054ff80b9bb6498aa081315f
kinggy <kinggy_hoo@hotmail.com>
2016-04-10 14:59:27 +0700

    Finish appRestaurant

--- app/src/main/java/pnu/ntc/b/appapinya/apinyaburisri/kinggyrestautant/ServiceActivity.java
+++ app/src/main/java/pnu/ntc/b/appapinya/apinyaburisri/kinggyrestautant/ServiceActivity.java
@@ -1,6 +1,9 @@
 package pnu.ntc.b.appapinya.apinyaburisri.kinggyrestautant;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.AsyncTask;
+import android.os.StrictMode;
 import android.support.annotation.StringRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,26 +14,33 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.Response;
 
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-public class ServiceActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.jar.Attributes;
 
+public class ServiceActivity extends AppCompatActivity {
 
     //Explicit
     private TextView textView;
     private Spinner spinner;
     private ListView listView;
 
-    private String officerString , deskString, foodString, amountString;
+    private String officerString, deskString, foodString, amountString;
     private String[] foodStrings, priceStrings, iconStrings;
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,11 +61,9 @@ public class ServiceActivity extends AppCompatActivity {
         MyConnectedFood myConnectedFood = new MyConnectedFood();
         myConnectedFood.execute();
 
-
     } //Main Method
 
     public class MyConnectedFood extends AsyncTask<Void, Void, String> {
-
 
         @Override
         protected String doInBackground(Void... voids) {
@@ -72,7 +80,6 @@ public class ServiceActivity extends AppCompatActivity {
                 return null;
             }
 
-
         }//doInBack
 
         @Override
@@ -88,8 +95,7 @@ public class ServiceActivity extends AppCompatActivity {
                 priceStrings = new String[jsonArray.length()];
                 iconStrings = new String[jsonArray.length()];
 
-
-                for (int i=0;i<jsonArray.length();i++) {
+                for (int i = 0; i < jsonArray.length(); i++) {
 
                     JSONObject jsonObject = jsonArray.getJSONObject(i);
                     foodStrings[i] = jsonObject.getString(MyManage.column_food);
@@ -105,13 +111,70 @@ public class ServiceActivity extends AppCompatActivity {
                         foodStrings, priceStrings, iconStrings);
                 listView.setAdapter(myAdapter);
 
+                listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                        chooseAmount(foodStrings[i]);
+
+                    }   //onItemClick
+                });
             } catch (Exception e) {
                 Log.d("10April", "Error strJSON ===>>> " + e.toString());
             }
-
         }//onPost
     }//MyConnectedFood Class
 
+    private void chooseAmount(String choosefoodString) {
+
+        foodString = choosefoodString;
+        CharSequence[] charSequence = {"1 จาน", "2 จาน", "3 จาน", "4 จาน", "5 จาน"};
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setIcon(R.drawable.icon_myaccount);
+        builder.setTitle(foodString);
+        builder.setSingleChoiceItems(charSequence, -1, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                amountString = Integer.toString(i + 1);
+                dialogInterface.dismiss();
+
+                updateToServer();
+
+            }   //onClick
+        });
+        builder.show();
+    }   //chooseAmount
+
+    private void updateToServer() {
+
+        //Connected HTTP
+        StrictMode.ThreadPolicy threadPolicy = new StrictMode.ThreadPolicy
+                .Builder().permitAll().build();
+        StrictMode.setThreadPolicy(threadPolicy);
+
+        try {
+            ArrayList<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+            nameValuePairs.add(new BasicNameValuePair("isAdd", "true"));
+            nameValuePairs.add(new BasicNameValuePair("Officer", officerString));
+            nameValuePairs.add(new BasicNameValuePair("Desk", deskString));
+            nameValuePairs.add(new BasicNameValuePair("Food", foodString));
+            nameValuePairs.add(new BasicNameValuePair("Amount", amountString));
+
+            HttpClient httpClient = new DefaultHttpClient();
+            HttpPost httpPost = new HttpPost("http://swiftcodingthai.com/9Apr/php_add_data_restaurant.php");
+            httpPost.setEntity(new UrlEncodedFormEntity(nameValuePairs, "UTF-8"));
+            httpClient.execute(httpPost);
+
+
+            Toast.makeText(this, "Update Order to Server แล้ว", Toast.LENGTH_SHORT).show();
+
+        } catch (Exception e) {
+
+            Toast.makeText(this, "ไม่สามารภเชื่อมต่อ Server ได้", Toast.LENGTH_SHORT).show();
+        }
+
+    }   //updateToServer
 
     private void createSpinner() {
         final String[] deskStrings = {"โต๊ะที่ 1", "โต๊ะที่ 2", "โต๊ะที่ 3", "โต๊ะที่ 4", "โต๊ะที่ 5", "โต๊ะที่ 6", "โต๊ะที่ 7", "โต๊ะที่ 8", "โต๊ะที่ 9", "โต๊ะที่ 10"};
@@ -128,6 +191,5 @@ public class ServiceActivity extends AppCompatActivity {
                 deskString = deskStrings[0];
             }
         });
-
     }
 } //Main Class
